38ba19116a8cff3b1bdafc6b09277266203fbd47
Ledokhovich Dmitry <89251254+BlackFrogKok@users.noreply.github.com>
2025-09-18 21:03:05 +0300

    Update puti.py

--- puti.py
+++ puti.py
@@ -4,12 +4,13 @@ from datetime import datetime as dt
 # fromisoformat()
 
 class Person:
-    def __init__(self, route_names, time_start, time_end, opr_time,
+    def __init__(self, route_names, time_start, time_end, opr_time, opr_start_date,
                  chanel, type_strah, damage, place, summ, grade):
         self.route_names: list = route_names
         self.time_start = time_start
         self.time_vipol = (time_end - time_start)
         self.opr_time = opr_time
+        self.opr_start_date = opr_start_date
         self.chanel = chanel
         self.type_strah = type_strah
         self.damage = damage
